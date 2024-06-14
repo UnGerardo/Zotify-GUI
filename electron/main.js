8ddde9caf3b9ce2d1ca939f3bad34860234dcc79
UnGerardo <gerardojdls@gmail.com>
2024-06-13 22:04:59 -0700

    Installed npm packages to assist with running electron and react, configured package.json with scripts to run both

---
+++ electron/main.js
@@ -0,0 +1,14 @@
+import { app, BrowserWindow } from 'electron';
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadURL('http://localhost:5173');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
