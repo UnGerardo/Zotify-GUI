988e05368cdcfdb66aba0fcbe69cab34033a13ba
UnGerardo <gerardojdls@gmail.com>
2024-06-14 23:14:22 -0700

    Added check to electron/main.js to open correct url/file when running dev or build. Installed packages and configured package.json with build info and build script.

--- electron/main.js
+++ electron/main.js
@@ -1,4 +1,15 @@
 import { app, BrowserWindow } from 'electron';
+import { isDev } from 'electron-is-dev';
+import { path, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { platform } from 'node:os';
+
+let currentDir = dirname(fileURLToPath(import.meta.url));
+currentDir = platform() === 'win32' ?
+  dirname(fileURLToPath(import.meta.url)).split('\\') :
+  dirname(fileURLToPath(import.meta.url)).split('/');
+currentDir.pop();
+const __dirname = currentDir.join('/');
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -6,7 +17,7 @@ const createWindow = () => {
     height: 600
   })
 
-  win.loadURL('http://localhost:5173');
+  win.loadURL(isDev ? 'http://localhost:5173' : `file://${path.join(__dirname, '../build/index.html')}`);
 }
 
 app.whenReady().then(() => {
