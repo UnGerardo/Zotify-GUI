5ec76fb205864fb7c617e94d7afde1846369e885
UnGerardo <gerardojdls@gmail.com>
2024-06-15 17:57:23 -0700

    Restarted project with electron-forge

--- README.md
+++
@@ -1,1 +0,0 @@
-# Zotify GUI
--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-import App from './App.jsx';
-import { createRoot }  from "react-dom/client";
-
-const domNode = document.getElementById('root');
-const root = createRoot(domNode);
-root.render(<App />)
