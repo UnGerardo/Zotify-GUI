4872acdfa0ffaf27af13f9ae0fc19231658c1797
UnGerardo <gerardojdls@gmail.com>
2024-06-18 19:10:58 -0700

    Get url and credentials and pass them to spawnZotify()

--- src/renderer.js
+++ src/renderer.js
@@ -1,5 +1,21 @@
 
-document.getElementById('b').addEventListener('click', async () => {
-  const zotifyOutput = await window.electronAPI.spawnZotify('TRACK_URL');
+const $spotifyUrl = document.querySelector('input[name="spotify-url"]');
+const $username = document.querySelector('input[name="username"]');
+const $password = document.querySelector('input[name="password"]');
+const $runZotifyBtn = document.getElementById('runZotify');
+
+$runZotifyBtn.addEventListener('click', async () => {
+  const spotifyUrl = $spotifyUrl.value;
+  const username = $username.value;
+  const password = $password.value;
+
+  const args = [
+    spotifyUrl,
+    `--username=${username}`,
+    `--password=${password}`,
+    '--print-downloads=True',
+  ];
+
+  const zotifyOutput = await window.electronAPI.spawnZotify(args);
   console.log(zotifyOutput);
 });
