716402138ead7e2f167046f92d4eefe5758ac959
UnGerardo <gerardojdls@gmail.com>
2024-06-20 16:47:59 -0700

    Got all param values and added to 'args'.

--- src/renderer.js
+++ src/renderer.js
@@ -4,6 +4,26 @@ const $runZotifyBtn = document.getElementById('runZotify');
 
 const $username = document.querySelector('input[name="username"]');
 const $password = document.querySelector('input[name="password"]');
+const $output = document.querySelector('input[name="output"]');
+const $songArchive = document.querySelector('input[name="song-archive"]');
+const $rootPath = document.querySelector('input[name="root-path"]');
+const $rootPodcastPath = document.querySelector('input[name="root-podcast-path"]');
+const $splitAlbumDiscs = document.querySelector('select[name="split-album-discs"]');
+const $downloadLyrics = document.querySelector('select[name="download-lyrics"]');
+const $mdAllGenres = document.querySelector('select[name="md-allgenres"]');
+const $mdGenreDelimiter = document.querySelector('input[name="md-genredelimiter"]');
+const $downloadFormat = document.querySelector('select[name="download-format"]');
+const $downloadQuality = document.querySelector('select[name="download-quality"]');
+const $transcodeBitrate = document.querySelector('input[name="transcode-bitrate"]');
+const $skipExistingFiles = document.querySelector('select[name="skip-existing-files"]');
+const $skipPreviouslyDownloaded = document.querySelector('select[name="skip-previously-downloaded"]');
+const $retryAttempts = document.querySelector('input[name="retry-attempts"]');
+const $bulkWaitTime = document.querySelector('input[name="bulk-wait-time"]');
+const $overrideAutoWait = document.querySelector('select[name="override-auto-wait"]');
+const $chunkSize = document.querySelector('input[name="chunk-size"]');
+const $downloadRealTime = document.querySelector('select[name="download-real-time"]');
+const $language = document.querySelector('select[name="language"]');
+const $tempDownloadDir = document.querySelector('input[name="temp-download-dir"]');
 
 const $downloads = document.getElementById('downloads');
 
@@ -11,19 +31,59 @@ $runZotifyBtn.addEventListener('click', async () => {
   const spotifyUrl = $spotifyUrl.value;
   const username = $username.value;
   const password = $password.value;
+  const output = $output.value;
+  const songArchive = $songArchive.value;
+  const rootPath = $rootPath.value;
+  const rootPodcastPath = $rootPodcastPath.value;
+  const splitAlbumDiscs = $splitAlbumDiscs.value;
+  const downloadLyrics = $downloadLyrics.value;
+  const mdAllGenres = $mdAllGenres.value;
+  const mdGenreDelimiter = $mdGenreDelimiter.value;
+  const downloadFormat = $downloadFormat.value;
+  const downloadQuality = $downloadQuality.value;
+  const transcodeBitrate = $transcodeBitrate.value;
+  const skipExistingFiles = $skipExistingFiles.value;
+  const skipPreviouslyDownloaded = $skipPreviouslyDownloaded.value;
+  const retryAttempts = $retryAttempts.value;
+  const bulkWaitTime = $bulkWaitTime.value;
+  const overrideAutoWait = $overrideAutoWait.value;
+  const chunkSize = $chunkSize.value;
+  const downloadRealTime = $downloadRealTime.value;
+  const language = $language.value;
+  const tempDownloadDir = $tempDownloadDir.value;
 
   const args = [
     spotifyUrl,
     `--username=${username}`,
     `--password=${password}`,
+    `--output=${output}`,
+    `--song-archive=${songArchive}`,
+    `--root-path=${rootPath}`,
+    `--root-podcast-path=${rootPodcastPath}`,
+    `--split-album-discs=${splitAlbumDiscs}`,
+    `--download-lyrics=${downloadLyrics}`,
+    `--md-allgenres=${mdAllGenres}`,
+    `--md-genredelimiter=${mdGenreDelimiter}`,
+    `--download-format=${downloadFormat}`,
+    `--download-quality=${downloadQuality}`,
+    `--transcode-bitrate=${transcodeBitrate}`,
+    `--skip-existing-files=${skipExistingFiles}`,
+    `--skip-previously-downloaded=${skipPreviouslyDownloaded}`,
+    `--retry-attempts=${retryAttempts}`,
+    `--bulk-wait-time=${bulkWaitTime}`,
+    `--override-auto-wait=${overrideAutoWait}`,
+    `--chunk-size=${chunkSize}`,
+    `--download-real-time=${downloadRealTime}`,
+    `--language=${language}`,
+    `--temp-download-dir=${tempDownloadDir}`,
     '--print-downloads=True',
   ];
 
-  const [ status, output ] = await window.electronAPI.spawnZotify(args);
+  const [ status, zotify ] = await window.electronAPI.spawnZotify(args);
 
   switch(status) {
     case 'Downloaded': {
-      const temp = output['stdout'].split('Downloaded "')[1];
+      const temp = zotify['stdout'].split('Downloaded "')[1];
       const endIndex = temp.indexOf('" to ');
       const trackInfo = temp.substring(0, endIndex);
 
@@ -32,7 +92,7 @@ $runZotifyBtn.addEventListener('click', async () => {
       break;
     }
     case 'SKIPPING': {
-      const temp = output['stdout'].split('SKIPPING: ')[1];
+      const temp = zotify['stdout'].split('SKIPPING: ')[1];
       const endIndex = temp.indexOf('(SONG ALREADY EXISTS)');
       const trackInfo = temp.substring(0, endIndex - 1);
 
@@ -41,7 +101,7 @@ $runZotifyBtn.addEventListener('click', async () => {
       break;
     }
     default:
-      $renderDownload(status, output['stderr'], status);
+      $renderDownload(status, zotify['stderr'], status);
   }
 });
 
