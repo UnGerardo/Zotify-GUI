40ebd281b18d3937d63fc100d27d65862c448171
UnGerardo <gerardojdls@gmail.com>
2024-06-23 16:08:34 -0700

    Fixed platform value by actually calling platform()

--- src/renderer/pathDefaults.js
+++ src/renderer/pathDefaults.js
@@ -1,7 +1,7 @@
 
 async function pathDefaults() {
   const homeDir = await window.electronAPI.homedir();
-  const platform = await window.electronAPI.platform;
+  const platform = await window.electronAPI.platform();
 
   $rootPath.value = platform === 'win32' ? `${homeDir}\\Zotify_Music` : `${homeDir}/Zotify_Music`;
   $rootPodcastPath.value = platform === 'win32' ? `${homeDir}\\Zotify_Podcasts` : `${homeDir}/Zotify_Podcasts`;
